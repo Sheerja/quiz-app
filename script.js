c5c60ce3ea84b3983d1225ee570fec0bcb8cf432
Sheerja <narangsheerja108@gmail.com>
2026-06-25 16:42:02 +0530

    Added project files

---
+++ script.js
@@ -0,0 +1,117 @@
+const nextbtn=document.getElementById("nextbtn");
+const current=document.getElementById("current");
+const options=document.querySelectorAll(".option");
+const dots=document.querySelectorAll(".dot");
+const ques=document.getElementById("question");
+
+let currentques=0;
+let score=0;
+const questions=[
+    {
+        question:"What is fullform of HTML?",
+        answers:[
+            "HyperText Markup Language",
+            "High Text Machine Language",
+            "Hyperlinks Text Mark Language",
+            "Home Tool Markup Language",
+        ],
+        correct:0
+    },
+    {
+        question:"Which css property is used to change the text color?",
+        answers:[
+            "font-size",
+            "color",
+            "font-color",
+            "color-text",
+        ],
+        correct:1,
+        userans:true
+    },
+    {
+        question:"Which symbol is used for id selector in css?",
+        answers:[
+             "#",
+              ".",
+              "&",
+             "*",
+         ],
+         correct:0
+    },
+    {
+        question:"Which property is used to make text bold?",
+        answers:[
+            "text-style",
+            "text-decoration",
+            "font-size",
+            "font-weight",
+        ],
+        correct:3
+    },
+    {
+        question:"Which HTML tag is used to insert an image?",
+        answers:[
+            "<image>",
+            "<picture>",
+            "<img>",
+            "<src>",
+        ],
+        
+        correct:2
+    }
+];
+ let answered = false;
+
+function showquestion(){
+           answered=false;
+            ques.innerText=questions[currentques].question;
+            current.innerText=currentques+1;
+
+            options.forEach((option,index)=>{
+               
+                option.innerText=String.fromCharCode(65+index)+"."+questions[currentques].answers[index];
+                 option.style.backgroundColor = "rgba(80, 2, 2, 0.521)";
+                
+            });
+            
+            dots.forEach(dot=>{
+                dot.classList.remove("active");
+            });
+            dots[currentques].classList.add("active");
+        }
+
+        options.forEach((option,index)=>{
+            option.addEventListener("click",()=>{
+                if(answered){
+                    return;
+                }
+                answered=true;
+                if(index===questions[currentques].correct)
+                {
+                    option.style.backgroundColor="green";
+                    score++;
+                }
+                else{
+                    option.style.backgroundColor="red";
+                    options[questions[currentques].correct].style.backgroundColor="green";
+                }
+            });
+        });
+        
+        nextbtn.addEventListener("click",()=>{
+            if(currentques<questions.length-1){
+                currentques++;
+                showquestion();
+            }
+            else{
+                ques.innerHTML = `
+                    🎉 Quiz Completed! 
+                    Your Score: ${score}/${questions.length}`;
+                 document.querySelector(".options").style.display = "none";
+
+        nextbtn.style.display = "none";
+            }
+        }
+        );
+
+showquestion();
